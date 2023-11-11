e3fcd89cbe350188cc4dac070af7c4b6211e45f6
inhasid <137834139+inhasid@users.noreply.github.com>
2023-11-11 14:42:06 +0000

    Deploying to gh-pages from @ inhasid/goit-js-hw-09@cf867a7a9e608b97e6e5cbf8bb7c28709d642600 🚀

---
+++ 01-color-switcher.0dac255b.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")},n=0;t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,t.btnStop.disabled=!1,n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.0dac255b.js.map
