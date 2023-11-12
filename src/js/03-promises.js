99e7701beb1d7bd5e3d7a200bcaa424f12953a51
inhasid <sidorchuk.inga@gmail.com>
2023-11-12 18:38:39 +0100

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+  
+function handleSubmit(event) {
+  
+  event.preventDefault();
+
+  const firstDelay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, firstDelay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  }); 
+}
+
